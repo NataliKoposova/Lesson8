cbb2d72b70e085a2ac027cdec27cc420944b3db1
Koposova1 <Koposova1@KOPOSOVANA>
2024-04-05 16:43:04 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Temp";
+            Console.WriteLine("Перечень подкаталогов:");
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string s in dirs)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Перечень файлов:");
+            string[] files = Directory.GetFiles(path,"*",SearchOption.AllDirectories);
+            foreach (string f in files)
+            {
+                Console.WriteLine(f);
+            }
+            Console.ReadKey();
+        }
+    }
+}  
